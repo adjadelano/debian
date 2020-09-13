2b400e7e6d94fa99d6f81c0b5b6010de160baab9
vibewill <vibewill@hotmail.com>
2020-09-13 17:45:38 -0300

    git

--- debian.sh
+++ debian.sh
@@ -1,4 +1,4 @@
-sudo apt install xorg bspwm rofi compton pulseaudio nitrogen build-essential rxvt-unicode cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev vim
+sudo apt install xorg bspwm rofi compton pulseaudio nitrogen build-essential git rxvt-unicode cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev vim
 mkdir .local
 mkdir .local/share 
 git clone htpps://github.com/vibewill/fonts
